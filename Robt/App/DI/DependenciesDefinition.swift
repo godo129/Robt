a397d110c6eef96dc996c820d3f6a81fbd4aad9f
godo129 <godo129@naver.com>
2023-03-12 22:26:00 +0900

    feat: DependencyDefinition 생성

---
+++ Robt/App/DI/DependenciesDefinition.swift
@@ -0,0 +1,14 @@
+//
+//  DependenciesDefinition.swift
+//  Robt
+//
+//  Created by hong on 2023/03/12.
+//
+
+import Foundation
+
+final class DependenciesDefinition {
+    func inject() {
+        let dependecies = DependenciesContainer.share
+    }
+}
