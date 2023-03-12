e1153900067be38e0be6b99fc0d4048a64521004
godo129 <godo129@naver.com>
2023-03-12 22:17:18 +0900

    feat: DependencyContainer 생성

---
+++ Robt/App/DI/DependenciesContainer.swift
@@ -0,0 +1,37 @@
+//
+//  DependenciesContainer.swift
+//  Robt
+//
+//  Created by hong on 2023/03/12.
+//
+
+import Foundation
+
+final class DependenciesContainer {
+    static let share = DependenciesContainer()
+    private var dependencies: [DependencyKey: Any] = [:]
+    
+    private init() {}
+    
+    func register<T>(
+        _ type: T.Type,
+        impl: Any,
+        name: String? = nil
+    ) {
+        let dependencyKey = DependencyKey(type: type, name: name)
+        dependencies[dependencyKey] = impl
+    }
+    
+    func resolve<T>(
+        _ type: T.Type,
+        name: String? = nil
+    ) -> T {
+        let dependencyKey = DependencyKey(type: type, name: name)
+        if let dep = dependencies[dependencyKey] as? T {
+            return dep
+        } else {
+            let protocolTypeName = NSString(string: "\(type)").components(separatedBy: ".").last!
+            fatalError("\(protocolTypeName) 의 존재성을 해결할 수 없습니다. 해당 실행 클래스: \(protocolTypeName).")
+        }
+    }
+}
