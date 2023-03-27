c94621ecf4a07ff55408227cbf1ec7bf965873ed
godo129 <godo129@naver.com>
2023-03-28 00:21:37 +0900

    feat: 홈 화면 뷰에서 로봇 버트 클릭시 채팅 뷰 이동기능

---
+++ Robt/Scene/Home/Main/ChatWithRobotViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ChatWithRobotViewController.swift
+//  Robt
+//
+//  Created by hong on 2023/03/28.
+//
+
+import UIKit
+
+final class ChatWithRobotViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+}
