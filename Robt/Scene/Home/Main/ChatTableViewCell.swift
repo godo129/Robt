eeb62067f404fe06e0c81534f8fd8e33909031f5
godo129 <godo129@naver.com>
2023-03-29 00:38:22 +0900

    feat: 테이블 뷰로 채팅 뷰 구성

---
+++ Robt/Scene/Home/Main/ChatTableViewCell.swift
@@ -0,0 +1,42 @@
+//
+//  ChatTableViewCell.swift
+//  Robt
+//
+//  Created by hong on 2023/03/29.
+//
+
+import UIKit
+
+final class ChatTableViewCell: UITableViewCell {
+
+    static let identifier = String(describing: ChatTableViewCell.self)
+    private let messageLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.numberOfLines = 0
+        label.textAlignment = .left
+        label.font = UIFont.systemFont(ofSize: 16)
+        label.textColor = .black
+        return label
+    }()
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        contentView.addSubview(messageLabel)
+        NSLayoutConstraint.activate([
+            messageLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 8),
+            messageLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16),
+            messageLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16),
+            messageLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -8)
+        ])
+    }
+
+    @available(*, unavailable)
+    required init?(coder _: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    func configure(with chatMessage: String) {
+        messageLabel.text = chatMessage
+    }
+}
--- Robt/Scene/Home/Main/ChatWithRobotViewController.swift
+++ Robt/Scene/Home/Main/ChatWithRobotViewController.swift
@@ -5,12 +5,64 @@
 //  Created by hong on 2023/03/28.
 //
 
+import SnapKit
+import Then
 import UIKit
 
 final class ChatWithRobotViewController: UIViewController {
 
+    private let chattingTableView: UITableView = .init()
+    private let chatTextField: UITextField = .init()
+
+    private let messages: [String] = ["ewafwefe", "awefwaeeawaefawefwewfewefaewfewfwee", "testetsesetsteetaetetwtwtewtawetaw\nawetweat\nwaetwaet\ntaeeat"]
+
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
+        configure()
+        chattingTableView.reloadData()
+    }
+}
+
+extension ChatWithRobotViewController {
+    func configure() {
+        [chattingTableView].forEach {
+            view.addSubview($0)
+        }
+
+        chattingTableView.register(
+            ChatTableViewCell.self,
+            forCellReuseIdentifier: ChatTableViewCell.identifier
+        )
+        layoutConfigure()
+        chattingTableView.delegate = self
+        chattingTableView.dataSource = self
+    }
+
+    func layoutConfigure() {
+        chattingTableView.snp.makeConstraints { make in
+            make.leading.trailing.equalToSuperview()
+            make.top.equalTo(view.safeAreaLayoutGuide).inset(50)
+            make.bottom.equalTo(view.safeAreaLayoutGuide).inset(50)
+        }
+    }
+}
+
+extension ChatWithRobotViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_: UITableView, numberOfRowsInSection _: Int) -> Int {
+        return messages.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(
+            withIdentifier: ChatTableViewCell.identifier,
+            for: indexPath
+        ) as? ChatTableViewCell else {
+            return UITableViewCell()
+        }
+        let message = messages[indexPath.row]
+        print(message)
+        cell.configure(with: message)
+        return cell
     }
 }
