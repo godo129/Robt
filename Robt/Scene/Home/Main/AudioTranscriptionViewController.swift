db6dc0b3ca614691344121db94dc3c5c1eedcd12
godo129 <godo129@naver.com>
2023-05-02 18:09:21 +0900

    feat: AudioTranscriptionView 생성

---
+++ Robt/Scene/Home/Main/AudioTranscriptionViewController.swift
@@ -0,0 +1,118 @@
+//
+//  AudioTranscriptionViewController.swift
+//  Robt
+//
+//  Created by hong on 2023/05/02.
+//
+
+import UIKit
+import Combine
+
+final class AudioTranscriptionViewController: UIViewController {
+    
+    private lazy var audioTitleView = UILabel().then {
+        $0.textColor = .black
+        $0.font = Font.semiBold(size: 18)
+        $0.layer.cornerRadius = 20
+        $0.layer.borderWidth = 2
+        $0.layer.borderColor = UIColor.purple.cgColor
+        $0.clipsToBounds = true
+        $0.text = "please select audio file"
+    }
+    private lazy var audioSelectButton = UIButton().then {
+        $0.layer.cornerRadius = 10
+        $0.setTitle("선택", for: .normal)
+        $0.setTitleColor(.OFFFFFF, for: .normal)
+        $0.setTitleColor(.OFFFFFF.withAlphaComponent(0.6), for: .highlighted)
+        $0.setBackgroundColor(.O000000, for: .normal)
+        $0.setBackgroundColor(.O000000.withAlphaComponent(0.6), for: .highlighted)
+        $0.titleLabel?.textAlignment = .center
+        $0.layer.cornerRadius = 10
+        $0.clipsToBounds = true
+        $0.titleLabel?.font = Font.bold(size: 18)
+    }
+    private lazy var transcriptionContainerView = UIScrollView()
+    private lazy var transcriptionStackView = UIStackView().then {
+        $0.axis = .vertical
+        $0.distribution = .fill
+        $0.spacing = 20
+    }
+    private lazy var transcriptionView = UILabel().then {
+        $0.textColor = .black
+        $0.font = Font.medium(size: 16)
+    }
+    
+    private let viewModel: AudioTrnascriptionViewModel
+    private var cancellabels: Set<AnyCancellable> = .init()
+    private var input: PassthroughSubject<AudioTrnascriptionViewModel.Input, Never> = .init()
+    
+    init(viewModel: AudioTrnascriptionViewModel) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configure()
+        bind()
+    }
+    
+    private func bind() {
+        let output = viewModel.transform(input: input.eraseToAnyPublisher())
+        output.sink { [weak self] event in
+            guard let self else {return}
+            switch event {
+            case let .audioFileUploaded(transcription):
+                print(transcription)
+            case .audioFileUploadFailed:
+                self.presentOKAlert(title: "오디오 추출에 실패하였습니다", message: "재시도 해주세요")
+            }
+        }
+        .store(in: &cancellabels)
+    }
+    
+}
+
+extension AudioTranscriptionViewController {
+    private func configure() {
+        title = "오디오 추출"
+        view.backgroundColor = .white
+        tabBarController?.setTabBarVisible(visible: false, duration: 0, animated: true)
+        navigationController?.setNavigationBarHidden(false, animated: true)
+        [audioTitleView, audioSelectButton, transcriptionContainerView].forEach {
+            view.addSubview($0)
+        }
+        transcriptionContainerView.addSubview(transcriptionStackView)
+        transcriptionStackView.addArrangedSubview(transcriptionView)
+        layoutConfigure()
+    }
+    
+    private func layoutConfigure() {
+        audioTitleView.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide).inset(50)
+            make.leading.equalToSuperview().inset(50)
+            make.trailing.equalToSuperview().inset(120)
+            make.height.equalTo(50)
+        }
+        audioSelectButton.snp.makeConstraints { make in
+            make.top.equalTo(audioTitleView.snp.top)
+            make.leading.equalTo(audioTitleView.snp.trailing).offset(40)
+            make.trailing.equalToSuperview().inset(50)
+            make.height.equalTo(audioTitleView.snp.height)
+        }
+        transcriptionContainerView.snp.makeConstraints { make in
+            make.top.equalTo(audioTitleView.snp.bottom).offset(50)
+            make.bottom.horizontalEdges.equalToSuperview().inset(50)
+        }
+        transcriptionStackView.snp.makeConstraints { make in
+            make.width.edges.equalToSuperview()
+        }
+        transcriptionView.snp.makeConstraints { make in
+            make.top.horizontalEdges.equalToSuperview().inset(50)
+        }
+    }
+}
---
+++ Robt/Scene/Home/Main/ViewModel/AudioTranscriptionViewModel.swift
@@ -0,0 +1,58 @@
+//
+//  AudioTranscriptionViewModel.swift
+//  Robt
+//
+//  Created by hong on 2023/05/02.
+//
+
+import Foundation
+import Combine
+
+final class AudioTrnascriptionViewModel: InputOutput {
+    
+    enum Input {
+        case audioFileUpload(_ path: String)
+    }
+    
+    enum Output {
+        case audioFileUploaded(_ transcription: String)
+        case audioFileUploadFailed
+    }
+    
+    var outPut: PassthroughSubject<Output, Never> = .init()
+    var cancellables: Set<AnyCancellable> = .init()
+    
+    private let usecase: AudioTranscriptionViewUsecaseProtocol
+    private weak var coordinator: MainCoordinator?
+    init(
+        usecase: AudioTranscriptionViewUsecaseProtocol,
+        coordinator: MainCoordinator
+    ) {
+        self.usecase = usecase
+        self.coordinator = coordinator
+    }
+    
+    func transform(input: AnyPublisher<Input, Never>) -> AnyPublisher<Output, Never> {
+        
+        input.sink { [weak self] event in
+            guard let self else {return}
+            switch event {
+            case let .audioFileUpload(path):
+                Task {
+                    do {
+                        let transcription = try await self.usecase.audioTranscription(audioFilePath:path)
+                        print("transcription은 다음과 같습니다 :", transcription)
+                        self.outPut.send(.audioFileUploaded(transcription))
+                    } catch {
+                        print(error)
+                        self.outPut.send(.audioFileUploadFailed)
+                    }
+                }
+            }
+        }
+        .store(in: &cancellables)
+        
+        return outPut.eraseToAnyPublisher()
+    }
+
+}
